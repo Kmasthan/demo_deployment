0bad96d433bd8effe6ef39d3309b47b4c888d933
Masthanaiah K <masthankalthireddy@gmail.com>
2025-05-24 15:41:58 +0530

    intial commit

---
+++ src/main/java/com/demo/demo_deployment/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.demo.demo_deployment.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MainController {
+
+	@GetMapping("/hello")
+	public String getMsg() {
+		return "Hello World!";
+	}
+}
---
+++ src/test/java/com/demo/demo_deployment/DemoDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.demo_deployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
